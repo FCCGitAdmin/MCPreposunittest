dffd3e11638beb4b1a17869738644c143efc1c6d
FCCGitAdmin <admin@M365CPI12967889.onmicrosoft.com>
2025-11-05 17:42:28 +0530

    Squash merge PR #8

---
+++ Testcopiot.js
@@ -0,0 +1,13 @@
+
+function greetUser(name) {
+    console.log(`Hello, ${name}! Welcome to JavaScript.`);
+}
+
+// Function to add two numbers
+function addNumbers(a, b) {
+    return a + b;
+}
+
+// Example usage
+greetUser("Narendran");
+console.log("Sum:", addNumbers(10, 20));
